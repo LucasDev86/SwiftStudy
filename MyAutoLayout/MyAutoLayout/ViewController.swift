a13c36be80ae82f3d03d5b14ab6d15736ac7a071
LucasDev86 <juseyo86@gmail.com>
2020-02-15 16:25:04 +0900

    Add files via upload
    오토레이아웃 예제

---
+++ MyAutoLayout/MyAutoLayout/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//
+//화면 레이아웃 배치 방법 2가지
+//1. 오토 리사이징(Auto Resizing) : 간단한 배치용도
+//2. 오토 레이아웃(Auto Layout) : 주로 사용함
+
+//*애플에서 새로운 UI 프레임워크 발표 : SwiftUI
+// XCODE 11.* MacOS 카탈리나 10.15 이상에서 동작
+// 향후 2-3년후에 보편화 될 전망임
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
